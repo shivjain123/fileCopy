b97c8459dcb369ced5e55f3752654591051711eb
Shiv Jain <70323345+shivjain123@users.noreply.github.com>
2021-05-21 17:22:40 +0530

    Update fileCopy.py

--- fileCopy.py
+++ fileCopy.py
@@ -1,19 +1,6 @@
-import os
 import shutil
 
-path = input("Please enter the file path.")
+sorucePath = input("Please enter the source path.")
+destinationPath = input("Please enter destination path.")
 
-List_Of_Files = os.listdir(path)
-
-print(List_Of_Files)
-
-for file in List_Of_Files:
-    splitF = os.path.splitext(file)
-    ext = splitF[1]
-    if (ext == " "):
-        continue
-    if os.path.exists(path + '/' + ext):
-        shutil.copy2(path + '/' + file, path + '/' + ext)
-    else:
-        os.makedirs(path + '/' + ext)
-        shutil.copy2(path + '/' + file, path + '/' + ext)
+shutil.copy2(sorucePath, destinationPath)
